0951a5b304936037cc1bf38e5d7c20ee0ebb2ed3
Dmitry <dmytro.shuba@nure.ua>
2019-10-28 21:01:15 +0200

    Minor updates

--- usermanagement/src/main/java/ua/nure/shuba/usermanagement/User.java
+++ usermanagement/src/main/java/ua/nure/shuba/usermanagement/User.java
@@ -5,94 +5,96 @@ import java.util.Date;
 import java.util.Objects;
 
 public class User {
-        private static final long serialVersionUID = -5553481350681901428L;
-        private Long id;
-        private String firstName;
-        private String lastName;
-        private Date dateOfBirth;
 
-        public Long getId() {
-            return id;
-        }
+    private static final long SERIAL_VERSION_UID = -5553481350681901428L;
 
-        public void setId(Long id) {
-            this.id = id;
-        }
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date birthdayDate;
 
-        public String getFirstName() {
-            return firstName;
-        }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects
+                .equals(firstName, user.firstName) && Objects
+                .equals(lastName, user.lastName) && Objects
+                .equals(birthdayDate, user.birthdayDate);
+    }
 
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, birthdayDate);
+    }
 
-        public String getLastName() {
-            return lastName;
-        }
+    public Long getId() {
+        return id;
+    }
 
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-        public Date getDateOfBirth() {
-            return dateOfBirth;
-        }
+    public String getFirstName() {
+        return firstName;
+    }
 
-        public void setDateOfBirth(Date dateOfBirth) {
-            this.dateOfBirth = dateOfBirth;
-        }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o)
-                return true;
-            if (o == null || getClass() != o.getClass())
-                return false;
-            User user = (User) o;
-            return Objects.equals(id, user.id) && Objects
-                    .equals(firstName, user.firstName) && Objects
-                    .equals(lastName, user.lastName) && Objects
-                    .equals(dateOfBirth, user.dateOfBirth);
-        }
+    public String getLastName() {
+        return lastName;
+    }
 
-        @Override
-        public int hashCode() {
-            return Objects.hash(id, firstName, lastName, dateOfBirth);
-        }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-        public String getFullName() {
-            return getLastName() + ", " + getFirstName();
-        }
+    public Date getBirthdayDate() {
+        return birthdayDate;
+    }
 
-        public int getAge() {
-            int age = 0;
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(new Date());
-            int currentYear = calendar.get(Calendar.YEAR);
-            int currentMonth = calendar.get(Calendar.MONTH);
-            int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
-            calendar.setTime(getDateOfBirth());
-            int birthYear = calendar.get(Calendar.YEAR);
-            int birthDay = calendar.get(Calendar.DAY_OF_MONTH);
-            int birthMonth = calendar.get(Calendar.MONTH);
-            int age1 = currentYear - birthYear;
-            int age2 = currentYear - birthYear - 1;
-            if (birthMonth < currentMonth) {
-                age = age1;
-            }
-            if (birthMonth > currentMonth) {
-                age = age2;
-            }
-            if (birthMonth == currentMonth && birthDay > currentDay) {
-                age = age2;
-            }
-            if (birthMonth == currentMonth && birthDay < currentDay) {
-                age = age1;
-            }
-            if (birthMonth == currentMonth && birthDay == currentDay) {
-                age = age1;
-            }
-            return age;
+    public void setBirthdayDate(Date birthdayDate) {
+        this.birthdayDate = birthdayDate;
+    }
+
+    public String getFullName() {
+        return getLastName() + ", " + getFirstName();
+    }
+
+    public int getAge() {
+        int age = 0;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+
+        int currentYear = calendar.get(Calendar.YEAR);
+        int currentMonth = calendar.get(Calendar.MONTH);
+        int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+        calendar.setTime(getBirthdayDate());
+        int birthYear = calendar.get(Calendar.YEAR);
+        int birthDay = calendar.get(Calendar.DAY_OF_MONTH);
+        int birthMonth = calendar.get(Calendar.MONTH);
+
+        int age1 = currentYear - birthYear;
+        int age2 = currentYear - birthYear - 1;
+
+        if (birthMonth < currentMonth) {
+            age = age1;
+        } else if (birthMonth > currentMonth) {
+            age = age2;
+        } else if (birthDay > currentDay) {
+            age = age2;
+        } else if (birthDay < currentDay) {
+            age = age1;
+        } else {
+            age = age1;
         }
+        return age;
+    }
 }
--- usermanagement/src/test/java/ua/nure/shuba/usermanagement/UserTest.java
+++ usermanagement/src/test/java/ua/nure/shuba/usermanagement/UserTest.java
@@ -5,31 +5,33 @@ import junit.framework.TestCase;
 import java.util.Calendar;
 
 public class UserTest extends TestCase {
-    private static final int CURRENT_YEAR = 2019;
-    private static final int YEAR_OF_BIRTH = 1999;
 
-    /* Data when birthday in the past*/
-    private static final int DAY_OF_BIRTH_PAST = 9;
+    // YEARS
+    private static final int CURRENT_YEAR = 2019;
+    private static final int YEAR_OF_BIRTH = 1998;
+
+    // Data when birthday in the past
+    private static final int DAY_OF_BIRTH_PAST = 8;
     private static final int MONTH_OF_BIRTH_PAST = Calendar.SEPTEMBER;
     private static final int AGE_PAST = CURRENT_YEAR - YEAR_OF_BIRTH;
 
-    /* Data when birthday today*/
-    private static final int DAY_OF_BIRTH_TODAY = 28;
+    // Data when birthday was yesterday
+    private static final int DAY_OF_BIRTH_YESTERDAY = 26;
+    private static final int MONTH_OF_BIRTH_YESTERDAY = Calendar.OCTOBER;
+    private static final int AGE_YESTERDAY = CURRENT_YEAR - YEAR_OF_BIRTH;
+
+    // Data when birthday today
+    private static final int DAY_OF_BIRTH_TODAY = 27;
     private static final int MONTH_OF_BIRTH_TODAY = Calendar.OCTOBER;
     private static final int AGE_TODAY = CURRENT_YEAR - YEAR_OF_BIRTH;
 
-    /* Data when birthday tomorrow*/
+    // Data when birthday tomorrow
     private static final int DAY_OF_BIRTH3 = 29;
     private static final int MONTH_OF_BIRTH3 = Calendar.OCTOBER;
     private static final int AGE_TOMORROW = CURRENT_YEAR - YEAR_OF_BIRTH - 1;
 
-    /* Data when birthday was yesterday */
-    private static final int DAY_OF_BIRTH_YESTERDAY = 27;
-    private static final int MONTH_OF_BIRTH_YESTERDAY = Calendar.OCTOBER;
-    private static final int AGE_YESTERDAY = CURRENT_YEAR - YEAR_OF_BIRTH;
-
-    /* Data when birthday in the future */
-    private static final int DAY_OF_BIRTH_FUTURE = 14;
+    // Data when birthday in the future
+    private static final int DAY_OF_BIRTH_FUTURE = 13;
     private static final int MONTH_OF_BIRTH_FUTURE = Calendar.DECEMBER;
     private static final int AGE_FUTURE = CURRENT_YEAR - YEAR_OF_BIRTH - 1;
 
@@ -44,24 +46,23 @@ public class UserTest extends TestCase {
         super.tearDown();
     }
 
-    public void testGetFullName() {
-        user.setFirstName("Nadya");
-        user.setLastName("Malytska");
-        assertEquals("Malytska, Nadya", user.getFullName());
-
+    public void testGetAgeIfBirthdayToday() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_TODAY, DAY_OF_BIRTH_TODAY);
+        user.setBirthdayDate(calendar.getTime());
+        assertEquals(AGE_TODAY, user.getAge());
     }
 
-    public void testGetAgeIfBirthdayInThePast() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_PAST, DAY_OF_BIRTH_PAST);
-        user.setDateOfBirth(calendar.getTime());
-        assertEquals(AGE_PAST, user.getAge());
+    public void testGetFullName() {
+        user.setFirstName("Dmytro");
+        user.setLastName("Shuba");
+        assertEquals("Shuba, Dmytro", user.getFullName());
     }
 
     public void testGetAgeIfBirthdayInTheFuture() {
         Calendar calendar = Calendar.getInstance();
         calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_FUTURE, DAY_OF_BIRTH_FUTURE);
-        user.setDateOfBirth(calendar.getTime());
+        user.setBirthdayDate(calendar.getTime());
         assertEquals(AGE_FUTURE, user.getAge());
     }
 
@@ -69,21 +70,21 @@ public class UserTest extends TestCase {
         Calendar calendar = Calendar.getInstance();
         calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_YESTERDAY,
                 DAY_OF_BIRTH_YESTERDAY);
-        user.setDateOfBirth(calendar.getTime());
+        user.setBirthdayDate(calendar.getTime());
         assertEquals(AGE_YESTERDAY, user.getAge());
-    }
-
-    public void testGetAgeIfBirthdayToday() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_TODAY, DAY_OF_BIRTH_TODAY);
-        user.setDateOfBirth(calendar.getTime());
-        assertEquals(AGE_TODAY, user.getAge());
     }
 
     public void testGetAgeIfBirthdayTomorrow() {
         Calendar calendar = Calendar.getInstance();
         calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH3, DAY_OF_BIRTH3);
-        user.setDateOfBirth(calendar.getTime());
+        user.setBirthdayDate(calendar.getTime());
         assertEquals(AGE_TOMORROW, user.getAge());
+    }
+
+    public void testGetAgeIfBirthdayInThePast() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_PAST, DAY_OF_BIRTH_PAST);
+        user.setBirthdayDate(calendar.getTime());
+        assertEquals(AGE_PAST, user.getAge());
     }
 }
